6fa61b4507ac0f116592b19ad5695d2b0e087d76
Lauren Walton <lolliewalton@gmail.com>
2019-11-17 23:19:18 +0100

    code cleanup, removed extranious int matrix

--- src/DLXPentominoDLUX.java
+++ src/DLXPentominoDLUX.java
@@ -1,10 +1,13 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Stack;
 
 /**
- *
+ * An Implementation of the Algorithm X with a Dancing Links structure where Pentominos
+ * and Dominos are fitted into a n x 7 area. Calculates the number of solutions very quickly
+ * until about n = 6, at which point it starts to slow down considerably.
+ * A table of already calculated values is printed at program start.
+ * Created for Week 6 exercises of Algo
+ * @author laurenwalton
  * Credit: Alois Heinz for the Algorithm X methods
  */
 public class DLXPentominoDLUX {
@@ -16,7 +19,6 @@ public class DLXPentominoDLUX {
 
   //Matrix variables
   private static DLXNode h = new DLXNode();
-  private static int[][] matrix;
   private static DLXNode[][] nodeSetup;
   private static int matrixRowCounter = 1; //row 0 is headers
   private static int cnt = 0;
@@ -34,6 +36,7 @@ public class DLXPentominoDLUX {
     }
 
     //not all shapes can fit in all sizes of board
+    //matrix gets an extra row for column headers
     int estTotalPositions;
     if (n > 2){
       estTotalPositions = 6*(n+2) + 7*(n-1) + 5*(n-2) + 10*(n-1) + 12*(n-2) + 24*(n-3) + 16*(n-1);
@@ -45,26 +48,20 @@ public class DLXPentominoDLUX {
 
     //first value is row (max value of different configurations of dominos/pentominos)
     //second value is column (as long as the board has spaces)
-    //matrices both get an extra row for column headers
-    matrix = new int[estTotalPositions][7*(n+1)];
-    nodeSetup = new DLXNode[estTotalPositions][7*(n+1)];
+    nodeSetup = new DLXNode[estTotalPositions][7*n];
 
-    System.out.println("Building the int Matrix...");
+    System.out.println("Building the Node Matrix...");
     buildHeaders(h);
-    calcPossibilities();
-
-    printMatrixHeaders();
-    printMatrix();
+    calcRows();
 
     System.out.println("Building the Dancing Links based on Matrix...");
     buildRows();
-    printNodeSetup();
+    //printNodeSetup();
 
     System.out.println("Starting to search for number of solutions...");
     search(0);
 
     System.out.println("The number of solutions for this size is: " + cnt);
-
 
   }
 
@@ -76,7 +73,6 @@ public class DLXPentominoDLUX {
       //make a new header and add it to the matrices
       DLXNode newHeader = new DLXNode();
       nodeSetup[0][i] = newHeader;
-      matrix[0][i] = 1;
 
       //on the left bind to previous node (initially the anchor node)
       if (i == 0){
@@ -103,7 +99,7 @@ public class DLXPentominoDLUX {
 
   //------------------------- Building The Rows -------------------------//
 
-  private static void calcPossibilities(){
+  private static void calcRows(){
 
     //for every row, fill in the possibilities - createMatrixEntry takes a list of board positions
     // eg {0, 1} would be a horizontal domino in the top left corner
@@ -123,8 +119,11 @@ public class DLXPentominoDLUX {
       //pentomino +
       if (row < n - 2) {
         for (int i = 0; i < 5; i++) {
-          createMatrixEntry(new int[]{rowStarter + i + 1, rowStarter + i + 7, rowStarter + i + 8,
-              rowStarter + i + 9, rowStarter + i + 15});
+          createMatrixEntry(new int[]{
+              rowStarter + i + 1,
+              rowStarter + i + 7, rowStarter + i + 8, rowStarter + i + 9,
+              rowStarter + i + 15
+          });
         }
       }
 
@@ -132,25 +131,33 @@ public class DLXPentominoDLUX {
       if (row < n - 2) {
         for (int i = 0; i < 6; i++) {
           //C shape
-          createMatrixEntry(new int[]{rowStarter + i, rowStarter + i + 1,
+          createMatrixEntry(new int[]{
+              rowStarter + i, rowStarter + i + 1,
               rowStarter + 7 + i,
-              rowStarter + 14 + i, rowStarter + 15 + i});
+              rowStarter + 14 + i, rowStarter + 15 + i
+          });
           //reverse C shape
-          createMatrixEntry(new int[]{rowStarter + i, rowStarter + i + 1,
+          createMatrixEntry(new int[]{
+              rowStarter + i, rowStarter + i + 1,
               rowStarter + 8 + i,
-              rowStarter + 14 + i, rowStarter + 15 + i});
+              rowStarter + 14 + i, rowStarter + 15 + i
+          });
         }
       }
 
       //pentomino C Wide
       if (row < n - 1) {
         for (int i = 0; i < 5; i++) {
-          //U shape
-          createMatrixEntry(new int[]{rowStarter + i, rowStarter + i + 2,
-              rowStarter + 7 + i, rowStarter + 8 + i, rowStarter + 9 + i});
+          //U shape |__|
+          createMatrixEntry(new int[]{
+              rowStarter + i, rowStarter + i + 2,
+              rowStarter + 7 + i, rowStarter + 8 + i, rowStarter + 9 + i
+          });
           //upside down U shape
-          createMatrixEntry(new int[]{rowStarter + i, rowStarter + i + 1, rowStarter + i +2,
-              rowStarter + 7 + i, rowStarter + 9 + i});
+          createMatrixEntry(new int[]{
+              rowStarter + i, rowStarter + i + 1, rowStarter + i +2,
+              rowStarter + 7 + i, rowStarter + 9 + i
+          });
         }
       }
 
@@ -158,85 +165,83 @@ public class DLXPentominoDLUX {
       if (row < n - 3) {
         for (int i = 0; i < 6; i++) {
           //L shape
-          createMatrixEntry(new int[]{rowStarter + i,
+          createMatrixEntry(new int[]{
+              rowStarter + i,
               rowStarter + i + 7,
               rowStarter + i + 14,
-              rowStarter + i + 21, rowStarter + i + 22});
+              rowStarter + i + 21, rowStarter + i + 22
+          });
 
           //backwards L shape
-          createMatrixEntry(new int[]{rowStarter + i + 1,
+          createMatrixEntry(new int[]{
+              rowStarter + i + 1,
               rowStarter + i + 8,
               rowStarter + i + 15,
-              rowStarter + i + 21, rowStarter + i + 22});
+              rowStarter + i + 21, rowStarter + i + 22
+          });
 
           //upsidedown L shape
-          createMatrixEntry(new int[]{rowStarter + i, rowStarter + i + 1,
+          createMatrixEntry(new int[]{
+              rowStarter + i, rowStarter + i + 1,
               rowStarter + i + 7,
               rowStarter + i + 14,
-              rowStarter + i + 21});
+              rowStarter + i + 21
+          });
 
           //upsidedown and backwards L shape
-          createMatrixEntry(new int[]{rowStarter + i, rowStarter + i + 1,
+          createMatrixEntry(new int[]{
+              rowStarter + i, rowStarter + i + 1,
               rowStarter + i + 8,
               rowStarter + i + 15,
-              rowStarter + i + 22});
+              rowStarter + i + 22
+          });
         }
-
       }
 
       //pentomino L Wide
       if (row < n - 1) {
         for (int i = 0; i < 4; i++) {
           // |___ shape
-          createMatrixEntry(new int[]{rowStarter + i,
-              rowStarter + i + 7, rowStarter + i + 8, rowStarter + i + 9, rowStarter + i + 10});
+          createMatrixEntry(new int[]{
+              rowStarter + i,
+              rowStarter + i + 7, rowStarter + i + 8, rowStarter + i + 9, rowStarter + i + 10
+          });
 
           // ___| shape
-          createMatrixEntry(new int[]{rowStarter + i + 3,
-              rowStarter + i + 7, rowStarter + i + 8, rowStarter + i + 9, rowStarter + i + 10});
+          createMatrixEntry(new int[]{
+              rowStarter + i + 3,
+              rowStarter + i + 7, rowStarter + i + 8, rowStarter + i + 9, rowStarter + i + 10
+          });
 
           // |--- shape
-          createMatrixEntry(new int[]{rowStarter + i, rowStarter + i + 1, rowStarter + i + 2, rowStarter + i + 3,
-              rowStarter + i + 7});
+          createMatrixEntry(new int[]{
+              rowStarter + i, rowStarter + i + 1, rowStarter + i + 2, rowStarter + i + 3,
+              rowStarter + i + 7
+          });
 
           // ---| shape
-          createMatrixEntry(new int[]{rowStarter + i, rowStarter + i + 1, rowStarter + i + 2, rowStarter + i + 3,
-              rowStarter + i + 10});
+          createMatrixEntry(new int[]{
+              rowStarter + i, rowStarter + i + 1, rowStarter + i + 2, rowStarter + i + 3,
+              rowStarter + i + 10
+          });
         }
-
       }
     }
   }
 
   private static void createMatrixEntry(int[] nodePositions){
     for (int i : nodePositions){
-      matrix[matrixRowCounter][i] = 1;
+      nodeSetup[matrixRowCounter][i] = new DLXNode();
     }
     matrixRowCounter++;
   }
 
   private static void buildRows(){
 
-    //Alt rewrite:
-    //create all relevant nodes and stick them in nodeSetupMatrix (unbound)
-    //single loop per row to link horizontally not including headers
-    //single loop per column starting with headers to link all nodes vertically & set column headers
-
-    for (int row=1; row < matrix.length; row++)
-    {
-      for (int col=0; col < matrix[row].length; col++)
-      {
-        if (matrix[row][col] == 1) {
-          DLXNode newNode = new DLXNode();
-          nodeSetup[row][col] = newNode;
-        }
-      }
-    }
-
     //link horizontally, every entry finds the next node right to connect to, headers already done with anchor
-    for (int row=1; row < nodeSetup.length; row++)
+    for (int row = 1; row < nodeSetup.length; row++)
     {
-      for (int col=0; col < nodeSetup[row].length; col++)
+      for (int col = 0; col < nodeSetup[row].length; col++)
       {
         if (nodeSetup[row][col] != null) {
           DLXNode currentNode = nodeSetup[row][col];
@@ -248,9 +253,9 @@ public class DLXPentominoDLUX {
     }
 
     //link vertically, every entry finds the next node up to connect to, and also set column header
-    for (int row=0; row < nodeSetup.length; row++)
+    for (int row = 0; row < nodeSetup.length; row++)
     {
-      for (int col=0; col < nodeSetup[row].length; col++)
+      for (int col = 0; col < nodeSetup[row].length; col++)
       {
         if (nodeSetup[row][col] != null) {
           DLXNode currentNode = nodeSetup[row][col];
@@ -262,72 +267,12 @@ public class DLXPentominoDLUX {
         }
       }
     }
-
   }
-
-  private static DLXNode createNewNode(int row, int col){
-    DLXNode newNode = new DLXNode();
-    nodeSetup[row][col] = newNode;
-
-    //link with neighbours, finding next node checks the int matrix, so row must be reduced by one
-    int rightColIndex = getNextRightNode(row, col);
-
-    //if there is another node in this row make links moving right
-    if (rightColIndex != col){
-/*      if (nodeSetup[row][leftColIndex] != null){
-        newNode.L = nodeSetup[row][leftColIndex];
-        nodeSetup[row][leftColIndex].R = newNode;
-      } else {
-        DLXNode leftNode = createNewNode(row, leftColIndex);
-        //newNode.L = leftNode;
-        //leftNode.R = newNode;
-      }*/
-      if (nodeSetup[row][rightColIndex] != null){
-        newNode.R = nodeSetup[row][rightColIndex];
-        nodeSetup[row][rightColIndex].L = newNode;
-      }else {
-         DLXNode rightNode = createNewNode(row, rightColIndex);
-         newNode.R = rightNode;
-         rightNode.L = newNode;
-      }
-    }
-    //there will always be at least one other node in the column due to the header, so no need to check
-    int nextUpIndex = getNextUpNode(row, col);
-    if (nodeSetup[nextUpIndex][col] != null){
-      newNode.U = nodeSetup[nextUpIndex][col];
-      nodeSetup[nextUpIndex][col].D = newNode;
-    }else {
-      DLXNode upNode = createNewNode(nextUpIndex, col);
-      //newNode.U = upNode;
-      //upNode.D = newNode;
-    }
-
-    int nextDownIndex = getNextDownNode(row, col);
-    if (nodeSetup[nextDownIndex][col] != null){
-      newNode.D = nodeSetup[nextDownIndex][col];
-      nodeSetup[nextDownIndex][col].U = newNode;
-    }else {
-      DLXNode downNode = createNewNode(nextDownIndex, col);
-      //newNode.D = downNode;
-      //downNode.U = newNode;
-    }
-
-    return newNode;
-  }
-
-/*  private static int getNextLeftNode(int row, int col){
-    for (int c = wrapLeft(col); c != col; c = wrapLeft(c)){
-      if (matrix[row][c] == 1){
-        return c;
-      }
-    }
-    return col;
-  }*/
 
   private static int getNextRightNode(int row, int col){
     int c = wrapRight(col);
     while (c != col){
-      if (matrix[row][c] == 1){
+      if (nodeSetup[row][c] != null){
         return c;
       } else {
         c = wrapRight(c);
@@ -339,7 +284,7 @@ public class DLXPentominoDLUX {
   private static int getNextUpNode(int row, int col){
     int r = wrapUp(row);
     while (r != row){
-      if (matrix[r][col] == 1){
+      if (nodeSetup[r][col] != null){
         return r;
       } else {
         r = wrapUp(r);
@@ -348,30 +293,12 @@ public class DLXPentominoDLUX {
     return row;
   }
 
-  private static int getNextDownNode(int row, int col){
-    for (int r = wrapDown(row); r != row; r = wrapDown(r)){
-      if (matrix[r][col] == 1){
-        return r;
-      }
-    }
-    return row;
-  }
-
-  private static int wrapLeft(int i){
-    return i == 0 ? matrix[0].length - 1 : i - 1;
-  }
-
   private static int wrapRight(int i){
-    return i == matrix[0].length - 1 ? 0 : i + 1;
+    return i == nodeSetup[0].length - 1 ? 0 : i + 1;
   }
 
-  //wrap up from 0 should be 6
   private static int wrapUp(int i){
-    return i == 0 ? matrix.length - 1 : i - 1;
-  }
-
-  private static int wrapDown(int i){
-    return i == matrix.length - 1 ? 0 : i + 1;
+    return i == 0 ? nodeSetup.length - 1 : i - 1;
   }
 
   //--------------------------- Algorithm X -----------------------------//
@@ -382,7 +309,7 @@ public class DLXPentominoDLUX {
    * all solutions to the exact cover problem encoded in the DLX matrix.
    * each time all columns are covered, static long cnt is increased
    *
-   * @param k: number of level
+   * @param k: number of levels (depth of search)
    */
   public static void search(int k) { // finds & counts solutions
     if (h.R == h) {
@@ -428,8 +355,8 @@ public class DLXPentominoDLUX {
    * @param c: header element of column that has to be uncovered
    */
   public static void uncover(DLXNode c) {//undo remove col c
-    for (DLXNode i = c.U; i != c; i = i.U)      // forall rows with 1
-      for (DLXNode j = i.L; i != j; j = j.L) {   // forall elem in row
+    for (DLXNode i = c.U; i != c; i = i.U)      // for all rows with 1
+      for (DLXNode j = i.L; i != j; j = j.L) {   // for all elem in row
         j.D.U = j;                       // un-remove row elem
         j.U.D = j;                       // .. to column list
       }
@@ -440,19 +367,13 @@ public class DLXPentominoDLUX {
   //-------------------- Printing Various Outputs -----------------------//
 
   private static void printTable(){
-    //TODO
-  }
+    int[] nValues = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8};
+    int[] solutions = new int[]{1, 0, 33, 160, 7483, 99742, 2367340, 36233454, 739687549};
 
-  private static void printMatrix(){
-    for (int[] row : matrix) {
-      for (int value : row) {
-        if (value == 1){
-          System.out.print(" +");
-        } else {
-          System.out.print("  ");
-        }
-      }
-      System.out.println();
+    System.out.println(" n Value | # Solutions");
+    System.out.println("--------------------------");
+    for (int i : nValues){
+      System.out.println("   " + i + "     | " + solutions[i]);
     }
   }
 
@@ -472,7 +393,6 @@ public class DLXPentominoDLUX {
       System.out.println();
     }
   }
-
 
   private static void printMatrixHeaders(){
     //anchor
@@ -501,7 +421,6 @@ public class DLXPentominoDLUX {
     System.out.println();
   }
 
-
   //---------------------- Accepting Valid Input ------------------------//
 
   private static int readInt(String s) {
@@ -509,7 +428,6 @@ public class DLXPentominoDLUX {
       try {
         System.out.print(s);
         System.out.flush();
-
         return Integer.parseInt(br.readLine().trim());
       } catch (Exception e) {
         System.out.println("That was not a valid integer.");
